27391884e8246c1a0b4846dd1acc78133d50632d
unknown <breveslol59@gmail.com>
2024-09-08 12:07:00 +1000

    project

--- news/views.py
+++ news/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 import requests
-
-API_KEY = "aa29f0bbe4a6457b8d224412ae53631a"
-
-
+import API_KEY
 def get_articles(category="all", lang="ru", pageSize=100):
     response = requests.get(
         f"https://newsapi.org/v2/everything?q={category}&language={lang}&pageSize={pageSize}&apiKey={API_KEY}"
@@ -16,7 +13,7 @@ def get_articles(category="all", lang="ru", pageSize=100):
 
 
 def homepage_view(request):
-    news = get_articles(category="Minecraft", pageSize=10)
+    news = get_articles(category="Minecraft", pageSize=12)
     return render(request, "index.html", {"news": news})
 
 
