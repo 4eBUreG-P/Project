163fe5dedbed9f30e08846c3a4731a4f18951789
4eBUreG-P <breveslol59@gmail.com>
2024-09-09 22:18:45 +1000

    Update wsgi.py

--- news_project/wsgi.py
+++ news_project/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for news_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
 
 import os
+import sys
 
+path = '/home/4eBUreG/project/news'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'news_project.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_project.settings")
+application = StaticFilesHandler(get_wsgi_application())
 
-application = get_wsgi_application()
